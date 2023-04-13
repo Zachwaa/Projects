f935831d4111218d8833d6ef710d73c0271615a9
Zachary Tonge <119691687+Zachwaa@users.noreply.github.com>
2023-04-13 12:26:07 +0100

    Delete server directory

--- server/routes/logout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post("/api/logout",(req,res,next) => {
-    req.session.destroy();
-        
-})
-
-module.exports = router;
