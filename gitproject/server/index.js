6f6c4111debdd2ba13800ee302df7183dc5a1bad
Zachary Tonge <119691687+Zachwaa@users.noreply.github.com>
2023-08-09 11:01:47 +0100

    Add files via upload

--- gitproject/server/index.js
+++ gitproject/server/index.js
@@ -1,44 +1,64 @@
 require('dotenv').config()
+
 const express = require("express");
+const app = express();
+
 const cookieParser = require("cookie-parser")
 const cors = require("cors");
-const sessions = require("express-session")
+const bodyParser = require("body-parser");
+const session = require("express-session")
 
 const logIn = require("./routes/login")
 const logout = require("./routes/logout")
-const bodyParser = require("body-parser");
-const app = express();
 const create = require("./routes/createNewAccount")
-//const path = require("path")
+const retrieve = require("./routes/searchProducts")
+
+const {  v1: uuidv1,} = require('uuid');
+const mysql = require('mysql');
+const MySQLStore = require('express-mysql-session')(session);
+
+const options = {
+  host:process.env.HOST,
+  user:process.env.USER,
+  password:process.env.PASSWORD,
+  database:process.env.DATABASE,
+}
+
+var sessionConnection = mysql.createConnection(options)
+const sessionStore = new MySQLStore({
+  expiration: 10800000,
+  createDatabaseTable: true,
+  clearExpired: false,
+  
+  schema: {
+    tableName: 'sessions',
+    columnNames: {
+        session_id: 'session_id',
+        expires: 'expires',
+        data: 'data'
+
+    }
+  }
+},sessionConnection)
+
+
 app.set("trust proxy",1)
-app.use(sessions({    // Creates a session with unique ID to be checked when user returns
+app.use(session({   
   secret: process.env.SECRET_KEY,
-  saveUninitialized:true,
-  cookie: { 
-      maxAge: parseInt(process.env.SESSION_MAX_AGE),
-      httpOnly:true,
-      path:"/",
-      secure:false,
-      sameSite:'lax'
-    },
-    
+  saveUninitialized:false,
+  store: sessionStore,
   resave: false,
 
-  user: "",
-  admin:false
+  cookie: { 
+      httpOnly:true,
+      maxAge: parseInt(process.env.SESSION_MAX_AGE),
+      id: uuidv1(),
+      path:"/",
+      secure:false,
+      sameSite:true
+    },
   
 })); 
-
-/*
-app.use(express.static(path.resolve(__dirname, '../client/build')));  //ONLY USE FOR PRODUCTION
-app.get("/",(req,res) => {
-  res.sendFile(path.join(__dirname, '/../react_dist', 'index.html'));
-})
-app.use((req,res,next) => {
-  console.log(req.session)
-  next()
-})
-*/
 
 app.use(
   cors({
@@ -53,6 +73,15 @@ app.use(bodyParser.json());
 app.use('/', logIn)
 app.use('/', create)
 app.use('/', logout)
+app.use('/',retrieve)
+app.post('/api',function(req,res,next){
+    if (req.session.admin == undefined){
+      req.session.admin = false
+      res.send(req.session)
+    } else {
+      res.send("SESSION ALREADY EXISTS")
+    }
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
